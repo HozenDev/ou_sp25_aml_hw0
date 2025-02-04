7ecea645f4adfdbe2655051dc082a9b63ce1f067
Hozen <enzo.durel@gmail.com>
2025-02-03 23:16:41 -0600

    up many

--- batch_many.sh
+++ batch_many.sh
@@ -36,4 +36,4 @@
 conda activate dnn
 
 # Change this line to start an instance of your experiment
-python hw0.py --hidden 64 32 16 --epochs 500 -vv --exp $SLURM_ARRAY_TASK_ID
+python hw0.py --hidden 128 64 32 --epochs 1000 --lrate 0.0005 -vv --exp $SLURM_ARRAY_TASK_ID
